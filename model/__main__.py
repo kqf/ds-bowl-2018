48187d4e2cc67bc61bc51d1a37d45b25d45b58aa
Oleksandr Kovalenko <oleksandr.kovalenko@growbots.com>
2018-03-03 18:43:21 +0100

    Add model evaluation

--- model/__main__.py
+++ model/__main__.py
@@ -1,6 +1,9 @@
 from model.data import TrainDataReader, ImageResizer, RleEncoder, ResultSaver, ShaperReporter, DataReader
 from model.cnn import CnnClassifier
 from model.timer import Timer
+from metrics import iou
+
+from sklearn.model_selection import train_test_split
 
 from sklearn.pipeline import make_pipeline
 def build_model():
@@ -14,12 +17,24 @@ def build_model():
 def main():
     with Timer('Prepare data'):
         train_imges, train_masks = TrainDataReader().read()
+        X_train, X_test, y_train, y_test = train_test_split(train_imges, train_masks, test_size=0.3, random_state=42)
 
     with Timer('Prepare data'):
         classifier = build_model()
 
     with Timer('Training the model'):
-        classifier.fit(train_imges, train_masks)
+        classifier.fit(X_train, X_train)
+
+    with Timer('Evaluate performance'):
+        train_predict = classifier.predict(X_train)
+        test_predict = classifier.predict(X_test)
+        print()
+        print("-------- Model evaluation -----------")
+        print("Score on training set:", iou(y_train, train_predict))
+
+        print("Score on test set:", iou(y_test, test_predict))
+        print("-------- ---------------- -----------")
+
 
     with Timer("Reading test set"):
         test_ids, test_images = DataReader().read()
