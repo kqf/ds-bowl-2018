1f6dd9971a218e327c4eb69f28cef97db122ed0c
Sasha <q.dev.null@gmail.com>
2020-08-11 18:33:34 +0200

    Add the timer for time tracking

--- model/main.py
+++ model/main.py
@@ -1,8 +1,19 @@
+import time
 import click
+from contextlib import contextmanager
 from pathlib import Path
 from model.data import PatchedDataset
 from model.data import CellsDataset
 from model.model import build_model
+
+
+@contextmanager
+def timer(name):
+    t0 = time.time()
+    yield
+    print("{color}[{name}] done in {et:.0f} s{nocolor}".format(
+        name=name, et=time.time() - t0,
+        color='\033[1;33m', nocolor='\033[0m'))
 
 
 @click.group()
@@ -17,7 +28,8 @@ def train(path):
     dataset = PatchedDataset(CellsDataset(dirs))
 
     model = build_model()
-    model.fit(dataset)
+    with timer("Train the model"):
+        model.fit(dataset)
 
 
 if __name__ == '__main__':
