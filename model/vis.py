05410834e25e55fe46c518b89a981a5acd7017ac
Sasha <q.dev.null@gmail.com>
2020-08-30 07:00:35 +0200

    Fix the plotting functions for binary tensors

--- model/vis.py
+++ model/vis.py
@@ -6,7 +6,7 @@ from torchvision.transforms.functional import to_pil_image
 def tensor2img(t, padding=16):
     std = torch.Tensor([0.229, 0.224, 0.225]).reshape(-1, 1, 1)
     mu = torch.Tensor([0.485, 0.456, 0.406]).reshape(-1, 1, 1)
-    img = to_pil_image(torch.tensor(t) * std + mu)
+    img = to_pil_image(torch.tensor(t) * std + mu if t.shape[0] > 1 else t)
     w, h = img.size
     return img.crop((padding, padding, w - padding, h - padding))
 
