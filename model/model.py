4a9d29ba606793d5f0ec9502e4157317cd96a933
Sasha <q.dev.null@gmail.com>
2020-09-01 08:02:09 +0200

    Resize the images to the same size when infering

--- model/model.py
+++ model/model.py
@@ -108,6 +108,7 @@ def train_transform():
 
 def test_transform():
     return alb.Compose([
+        alb.Resize(256, 256),
         alb.Normalize(
             mean=[0.485, 0.456, 0.406],
             std=[0.229, 0.224, 0.225],
