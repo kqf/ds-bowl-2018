fdea58330bb262b840b170f2e0241a3f83781885
Sasha <q.dev.null@gmail.com>
2020-10-11 15:07:49 +0200

    Fix the formatting for metrics module

--- model/metrics.py
+++ model/metrics.py
@@ -17,11 +17,15 @@ def _iou_single(y_pred, labels, verbose):
         print("Number of predicted objects:", pred_objects)
 
     # Compute intersection between all objects
-    intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0]
+    intersection = np.histogram2d(
+        labels.flatten(),
+        y_pred.flatten(),
+        bins=(true_objects, pred_objects)
+    )[0]
 
     # Compute areas (needed for finding the union between all objects)
-    area_true = np.histogram(labels, bins = true_objects)[0]
-    area_pred = np.histogram(y_pred, bins = pred_objects)[0]
+    area_true = np.histogram(labels, bins=true_objects)[0]
+    area_pred = np.histogram(y_pred, bins=pred_objects)[0]
     area_true = np.expand_dims(area_true, -1)
     area_pred = np.expand_dims(area_pred, 0)
 
@@ -29,8 +33,8 @@ def _iou_single(y_pred, labels, verbose):
     union = area_true + area_pred - intersection
 
     # Exclude background from the analysis
-    intersection = intersection[1:,1:]
-    union = union[1:,1:]
+    intersection = intersection[1:, 1:]
+    union = union[1:, 1:]
     union[union == 0] = 1e-9
 
     # Compute the intersection over union
@@ -42,20 +46,23 @@ def _iou_single(y_pred, labels, verbose):
         true_positives = np.sum(matches, axis=1) == 1   # Correct objects
         false_positives = np.sum(matches, axis=0) == 0  # Missed objects
         false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
-        tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+        tp, fp, fn = np.sum(true_positives), np.sum(
+            false_positives), np.sum(false_negatives)
         return tp, fp, fn
 
     # Loop over IoU thresholds
     prec = []
     if verbose:
         print("Thresh\tTP\tFP\tFN\tPrec.")
+
     for t in np.arange(0.5, 1.0, 0.05):
         tp, fp, fn = precision_at(t, iou)
         p = tp / (tp + fp + fn)
         if verbose:
             print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tp, fp, fn, p))
         prec.append(p)
-        
+
     if verbose:
         print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+
     return np.mean(prec)
