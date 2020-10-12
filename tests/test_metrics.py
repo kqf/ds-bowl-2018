34cd5f425f9a81ce57e368a346cd5166054d2219
Sasha <q.dev.null@gmail.com>
2020-10-12 07:43:22 +0300

    Simplify the tests for the metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -5,10 +5,11 @@ from model.metrics import iou
 
 
 @pytest.mark.parametrize("true_labels, predicted", [
-    (np.random.randn(2, 256, 256) > 0.5, np.random.randn(2, 256, 256) > 0.5),
-    (np.random.randn(3, 256, 256) > 0.5, np.random.randn(2, 256, 256) > 0.5),
-    (np.random.randn(2, 256, 256) > 0.5, np.random.randn(3, 256, 256) > 0.5),
-    (np.zeros((2, 256, 256)), np.random.randn(3, 256, 256) > 0.5),
+    (np.random.randn(2, 25, 25) > 0.5, np.random.randn(2, 25, 25)),
+    (np.random.randn(3, 25, 25) > 0.5, np.random.randn(2, 25, 25)),
+    (np.random.randn(2, 25, 25) > 0.5, np.random.randn(3, 25, 25)),
+    (np.zeros((2, 25, 25)), np.random.randn(3, 25, 25)),
 ])
 def test_calculates_metrics(true_labels, predicted):
-    assert iou(true_labels.astype(int), predicted.astype(int)) == 0.0
+    y_true = true_labels.astype(int)
+    assert iou(y_true, predicted) == 0.0
