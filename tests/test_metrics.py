16d35e40a852c7d28abf90286dd8cb98ecdd4738
Sasha <q.dev.null@gmail.com>
2020-10-14 08:09:11 +0300

    Fix the positive exmaple for iou

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -4,39 +4,38 @@ import numpy as np
 from model.metrics import iou, iou_approx
 
 
-@pytest.mark.parametrize("true_labels, predicted", [
+@pytest.mark.parametrize("true_labels, logits", [
     (np.random.randn(2, 25, 25) > 0.5, np.random.randn(2, 25, 25)),
     (np.random.randn(3, 25, 25) > 0.5, np.random.randn(2, 25, 25)),
     (np.random.randn(2, 25, 25) > 0.5, np.random.randn(3, 25, 25)),
     (np.zeros((2, 25, 25)), np.random.randn(3, 25, 25)),
 ])
-def test_calculates_metrics(true_labels, predicted):
+def test_calculates_metrics(true_labels, logits):
     y_true = true_labels.astype(int)
-    assert iou(y_true, predicted) == 0.0
+    assert iou(y_true, logits) == 0.0
 
 
-@pytest.mark.parametrize("true_labels, predicted", [
+@pytest.mark.parametrize("true_labels, logits", [
     (np.random.randn(3, 25, 25) > 0.5, np.random.randn(3, 25, 25)),
     (np.random.randn(3, 25, 25) > 0.5, np.random.randn(3, 25, 25)),
     (np.random.randn(3, 25, 25) > 0.5, np.random.randn(3, 25, 25)),
 ])
-def test_calculates_approx_metrics(true_labels, predicted):
+def test_calculates_approx_metrics(true_labels, logits):
     y_true = true_labels.astype(int)
-    assert iou_approx(y_true, predicted, padding=1) > 0.0
+    assert iou_approx(y_true, logits, padding=1) > 0.0
 
 
-@pytest.mark.parametrize("true_labels, predicted", [
+@pytest.mark.parametrize("true_labels, logits", [
     (np.zeros((1, 25, 25)), np.ones((1, 25, 25))),
     (np.zeros((3, 25, 25)), np.random.randn(3, 25, 25)),
 ])
-def test_calculates_minimum_approx_metrics(true_labels, predicted):
+def test_calculates_minimum_approx_metrics(true_labels, logits):
     y_true = true_labels.astype(int)
-    assert iou_approx(y_true, predicted) == 0.
+    assert iou_approx(y_true, logits) == 0.
 
 
-@pytest.mark.parametrize("true_labels, predicted", [
-    (np.eye(25, 25), np.eye(25, 25)),
-])
-def test_calculates_maximum_approx_metrics(true_labels, predicted):
-    y_true = true_labels.astype(int)
-    assert iou_approx(y_true[None, :, :], predicted[None, :, :]) == 1.
+def test_calculates_maximum_approx_metrics():
+    y_true = np.eye(25, 25)[None, :, :]
+    # Convert [0, 1] -> [-999, 999]
+    logits = (y_true - 0.5) * 999 * 2
+    assert iou_approx(y_true, logits) == 1.
