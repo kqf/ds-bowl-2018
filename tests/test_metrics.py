c4dfff6f4424127e5dbbf1aad47e57768ed5ca7c
Sasha <q.dev.null@gmail.com>
2020-10-14 06:46:51 +0300

    Add the test metrics with nontrivial outcome

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -19,16 +19,16 @@ def test_calculates_metrics(true_labels, predicted):
     (np.random.randn(3, 25, 25) > 0.5, np.random.randn(3, 25, 25)),
     (np.random.randn(3, 25, 25) > 0.5, np.random.randn(3, 25, 25)),
     (np.random.randn(3, 25, 25) > 0.5, np.random.randn(3, 25, 25)),
-    (np.zeros((3, 25, 25)), np.random.randn(3, 25, 25)),
 ])
 def test_calculates_approx_metrics(true_labels, predicted):
     y_true = true_labels.astype(int)
-    assert np.isnan(iou_approx(y_true, predicted))
+    assert iou_approx(y_true, predicted, padding=1) > 0.0
 
 
 @pytest.mark.skip("TODO: fix me")
 @pytest.mark.parametrize("true_labels, predicted", [
-    (np.ones((3, 25, 25)), np.ones((3, 25, 25))),
+    (np.zeros((1, 25, 25)), np.ones((1, 25, 25))),
+    (np.zeros((3, 25, 25)), np.random.randn(3, 25, 25)),
 ])
 def test_calculates_maximum_approx_metrics(true_labels, predicted):
     y_true = true_labels.astype(int)
